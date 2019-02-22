0c1b7ac87159ebc2925e760c9cfcf0c638941030
Rosco Kalis <roscokalis@gmail.com>
2019-02-22 13:28:16 +0100

    Changed the database interface to a class, taking a database url and client class as args * This way, it can be more easily configured, swapped out, or mocked for tests * Also added dotenv and sample configuration

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
-const port = 3000
-const db = require('./lib/queries')
+const { Db } = require('./lib/db')
+
+require('dotenv').config();
+const env = process.env.NODE_ENV || 'development'
+const port = process.env.PORT || 3000
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017'
+
+const db = new Db(mongoUrl);
 
 app.use(bodyParser.json())
 app.use(
@@ -21,7 +28,7 @@ app.get('/recipes', db.getRecipes)
 app.get('/recipes/:id', db.getRecipeById)
 app.post('/recipes', db.createRecipe)
 app.put('/recipes/:id', db.updateRecipe)
-app.delete('/recipes/:id', db.deletedeleteRecipeTodo)
+app.delete('/recipes/:id', db.deleteRecipe)
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
