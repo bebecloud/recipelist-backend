c0c46b648a0154e0dbe6254532dda68fc915810b
Rosco Kalis <roscokalis@gmail.com>
2019-02-24 17:02:54 +0100

    Added db.getRecipes with pagination and tests

--- test/db.test.js
+++ test/db.test.js
@@ -30,6 +30,10 @@ describe('createRecipe', () => {
     }
   })
 
+  after(async () => {
+    await db.recipes.remove()
+  })
+
   it('should fail when not passing ingredients', async () => {
     // given
     req.body = {
@@ -44,6 +48,7 @@ describe('createRecipe', () => {
 
     // then
     sinon.assert.calledWith(res.sendStatus, 400)
+    sinon.assert.notCalled(res.json)
   })
 
   it('should add recipe to the database', async () => {
@@ -58,12 +63,90 @@ describe('createRecipe', () => {
 
     // when
     await db.createRecipe(req, res)
-    console.log(res.json.firstCall.args)
 
     // then
     sinon.assert.calledWith(res.status, 201)
     sinon.assert.calledWithMatch(res.json, req.body.recipe)
   })
 })
+
+describe('getAllRecipes', () => {
+  let req = {}
+  let res = {}
+
+  before(async () => {
+    res = {
+      json: sinon.stub().returnsThis(),
+      status: sinon.stub().returnsThis(),
+      send: sinon.stub().returnsThis(),
+      sendStatus: sinon.stub().returnsThis()
+      // Add additional methods if using them in the API
+    }
+    req = {
+      body: {
+        recipe: {
+          ingredients: ['Potatoes'],
+          instructions: 'Cook potatoes'
+        }
+      },
+      params: {}
+    }
+
+    // populate db with five recipes TODO: Replace with fixtures
+
+    for (let i = 0; i < 5; i++) {
+      await db.createRecipe(req, res)
+    }
+  })
+
+  beforeEach(async () => {
+    res = {
+      json: sinon.stub().returnsThis(),
+      status: sinon.stub().returnsThis(),
+      send: sinon.stub().returnsThis(),
+      sendStatus: sinon.stub().returnsThis()
+      // Add additional methods if using them in the API
+    }
+    req = {
+      body: {},
+      params: {}
+    }
+  })
+
+  after(async () => {
+    await db.recipes.remove()
+  })
+
+  it('should return all recipes', async () => {
+    // when
+    await db.getRecipes(req, res)
+
+    // then
+    sinon.assert.calledWith(res.status, 200)
+    sinon.assert.calledWith(res.json, sinon.match.array)
+
+    const recipes = res.json.firstCall.args[0]
+    assert.equal(recipes.length, 5)
+  })
+
+  it('should return subset of recipes', async () => {
+    // given
+    req.body = {
+      pageSize: 2,
+      pageNumber: 2
+    }
+
+    // when
+    await db.getRecipes(req, res)
+
+    // then
+    sinon.assert.calledWith(res.status, 200)
+    sinon.assert.calledWith(res.json, sinon.match.array)
+
+    const recipes = res.json.firstCall.args[0]
+    assert.equal(recipes.length, 2)
+  })
+})
+
 
 // ... etc
